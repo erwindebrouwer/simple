2e22bb3b47e2eef6e4b6f1dd4a295778d6cb019b
Erwin <erwin@ubuntu.local>
2020-12-04 17:05:25 +0100

    Final change addEq100

--- simple.py
+++ simple.py
@@ -1,8 +1,8 @@
 # simple.py
 number = input("Number: ")
-if number > 100:
+if int(number) > 100:
     print("Wow! That's higher than 100!")
-if number < 100:
+if int(number) < 100:
     print("Oops... That's lower than 100.")
-if number == 100:
+if int(number) == 100:
     print("No way! That's exactly 100!")
