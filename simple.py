be432f29f233dc7ac18d8137e1590b21b64489e7
Erwin <erwin@ubuntu.local>
2020-12-04 16:48:54 +0100

    added a number check

--- simple.py
+++ simple.py
@@ -1,5 +1,17 @@
 # simple.py
+import sys
+#
+# get input
 number = input("Number: ")
+#
+# Check if input is a number
+try:
+    int(number)
+except:
+    print("ERROR: Your input is not a number!")
+    sys.exit()
+#
+# print analysis
 if number > 100:
     print("Wow! That's higher than 100!")
 if number < 100:
