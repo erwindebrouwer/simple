74dcd34fb26565adf01bf471a30dd65b62e2878b
Erwin <erwin@ubuntu.local>
2020-12-04 17:03:11 +0100

    testing addEq100

--- simple.py
+++ simple.py
@@ -4,3 +4,5 @@ if number > 100:
     print("Wow! That's higher than 100!")
 if number < 100:
     print("Oops... That's lower than 100.")
+if number == 100:
+    print("No way! That's exactly 100!")
