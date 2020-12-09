00781bf8efacd23ddd23ba09ed8a2be0b02876ac
Erwin <erwin@ubuntu.local>
2020-12-09 19:32:58 +0100

    added functions to the code

--- simple.py
+++ simple.py
@@ -1,20 +1,24 @@
 # simple.py
 import sys
 #
+def Check_Number(number):
+    # Check if input is a number
+    try:
+        int(number)
+    except:
+        print("ERROR: Your input is not a number!")
+        sys.exit()
+#
+def Analyze_Number(number):
+    # print analysis
+    if int(number) > 100:
+        print("Wow! That's higher than 100!")
+    if int(number) < 100:
+        print("Oops... That's lower than 100.")
+    if int(number) == 100:
+        print("No way! That's exactly 100!")
+#
 # get input
 number = input("Number: ")
-#
-# Check if input is a number
-try:
-    int(number)
-except:
-    print("ERROR: Your input is not a number!")
-    sys.exit()
-#
-# print analysis
-if int(number) > 100:
-    print("Wow! That's higher than 100!")
-if int(number) < 100:
-    print("Oops... That's lower than 100.")
-if int(number) == 100:
-    print("No way! That's exactly 100!")
+Check_Number(number)
+Analyze_Number(number)
