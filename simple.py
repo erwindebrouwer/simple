fc806416b45637e685d753839b574d1521358526
Erwin <erwin@ubuntu.local>
2020-12-09 20:16:46 +0100

    add returns to the def's

--- simple.py
+++ simple.py
@@ -12,14 +12,15 @@ def Check_Number(number):
 def Analyze_Number(number):
     # print analysis
     if int(number) > 100:
-        print("Wow! That's higher than 100!")
+        return "Wow! That's higher than 100!"
     if int(number) < 100:
-        print("Oops... That's lower than 100.")
+        return "Oops... That's lower than 100."
     if int(number) == 100:
-        print("No way! That's exactly 100!")
+        return "No way! That's exactly 100!"
 #
 if __name__ == "__main__":
     # get input
     number = input("Number: ")
     Check_Number(number)
-    Analyze_Number(number)
+    conclusion = Analyze_Number(number)
+    print(conclusion)
